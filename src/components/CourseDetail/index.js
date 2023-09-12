265c540fdc847f3542cd87290d9c0e20a1a74313
blacksecrettoken <secretsuperdev9472@gmail.com>
2023-09-12 09:05:43 -0700

    fix coursedetail component

--- src/components/CourseDetail/index.js
+++ src/components/CourseDetail/index.js
@@ -3,7 +3,18 @@ import { useState } from "react";
 
 export default function index(props) {
   const { courseNo, prev } = props;
-  const numberArr = ['One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten'];
+  const numberArr = [
+    "One",
+    "Two",
+    "Three",
+    "Four",
+    "Five",
+    "Six",
+    "Seven",
+    "Eight",
+    "Nine",
+    "Ten",
+  ];
   const [level, setLevel] = useState("");
   const [name, setName] = useState("");
   const [skill, setSkill] = useState("");
@@ -37,7 +48,7 @@ export default function index(props) {
             Course
           </h5>
           <h3 className="text-4xl font-bold text-gray-900 dark:text-white">
-            {numberArr[courseNo-1]}
+            {numberArr[courseNo - 1]}
           </h3>
         </div>
         <div>
@@ -115,18 +126,41 @@ export default function index(props) {
           />
         </div>
         <div className="flex flex-row gap-1">
-          <span onClick={() => prev()} className="text-white text-[30px] w-[112px] pt-[7px] cursor-pointer bg-slate-300 hover:bg-main focus:ring-main focus:outline-none focus:bg-main font-medium rounded-[100px] text-sm text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          <span
+            onClick={() => prev()}
+            className="text-white text-[30px] w-[112px] pt-[7px] cursor-pointer bg-slate-300 hover:bg-main focus:ring-main focus:outline-none focus:bg-main font-medium rounded-[100px] text-sm text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
             &lt;
           </span>
           <button
             type="submit"
-            className="w-full text-white bg-main hover:bg-purple-800 focus:ring-purple-800 focus:outline-none focus:bg-main font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            className="flex flex-row items-center gap-2 w-full text-white bg-main hover:bg-purple-800 focus:ring-purple-800 focus:outline-none focus:bg-main font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
-            Add Course
+            <span>
+              Add Course
+            </span>
+            <span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="text-white"
+                viewBox="0 0 16 16"
+              >
+                {" "}
+                <path
+                  d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"
+                  fill="white"
+                ></path>{" "}
+                <path
+                  d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"
+                  fill="white"
+                ></path>{" "}
+              </svg>
+            </span>
           </button>
-          <button
-            className="w-full text-white bg-main hover:bg-purple-800 focus:ring-purple-800 focus:outline-none focus:bg-main font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
+          <button className="w-full text-white bg-main hover:bg-purple-800 focus:ring-purple-800 focus:outline-none focus:bg-main font-medium rounded-lg text-sm px-2.5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             Submit
           </button>
         </div>
